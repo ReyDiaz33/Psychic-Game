1c1e5e6509faf5947dc2ee0d540402505619b571
Rey Diaz <reybdiazjr@gmail.com>
2019-08-01 22:58:44 -0400

    added a little to style to help it match the file looks and added to some of the JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,9 @@ var losses = 0;
 var guessesLeft = 9;
 var letersGuessed = [];
 var computerGuess = []; 
+
+window.onload = function(){
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerguess[0]);
+}
