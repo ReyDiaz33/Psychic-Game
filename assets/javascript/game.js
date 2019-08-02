d63768323519bf8f5a9e699b709904cc99160042
Rey Diaz <reybdiazjr@gmail.com>
2019-08-01 22:35:08 -0400

    Added array of computer choices in JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+// all the possible chices the computer can make
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", 
+"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
