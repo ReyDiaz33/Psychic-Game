bb1ce2cf2f8ec683ab1f4c195229455631f38afe
Rey Diaz <reybdiazjr@gmail.com>
2019-08-01 22:39:41 -0400

    added variables of wins losses and guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,9 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", 
 "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// wins losses guesses left and computer guesses
+var win = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letersGuessed = [];
+var computerGuess = []; 
